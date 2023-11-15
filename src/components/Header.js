5131ee8a35a9017496e24ae64aee0c1c9eaf79ba
Leonardo Basualdo <ljbasualdo@gmail.com>
2023-11-15 12:11:07 -0300

    231115

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = props =>{
+    return(
+        <p>Cabecera</p>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return(
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/nosotros">Nosotros</Link></li>
+                <li><Link to="/servicios">Servicios</Link></li>
+                <li><Link to="/galeria">Galería</Link></li>
+                <li><Link to="/contacto">Contacto</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
