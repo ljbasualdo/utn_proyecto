b14f2bcf0af85171923f3be1c63319ec4c108944
Leonardo Basualdo <ljbasualdo@gmail.com>
2023-11-24 10:30:40 -0300

    Actualización Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = (props) => {
     return(
         <nav>
             <ul className="holder">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/nosotros">Nosotros</Link></li>
-                <li><Link to="/servicios">Servicios</Link></li>
-                <li><Link to="/galeria">Galería</Link></li>
-                <li><Link to="/contacto">Contacto</Link></li>
+                <li><NavLink className={({ isActive }) => isActive ? "activo" : undefined} to="/">Home</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? "activo" : undefined} to="/nosotros">Nosotros</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? "activo" : undefined} to="/servicios">Servicios</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? "activo" : undefined} to="/galeria">Galería</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? "activo" : undefined} to="/contacto">Contacto</NavLink></li>
             </ul>
         </nav>
     )
